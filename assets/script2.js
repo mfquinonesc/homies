dbccc734f6a0347eb70b14dd34e7d2a67e7d04dc
mfquinonesc <manuelfqc@gmail.com>
2022-04-29 11:36:04 -0500

    carousel

--- assets/script2.js
+++ assets/script2.js
@@ -1,3 +1,71 @@
+let btn_left = document.getElementById("btn-left");
+let btn_right = document.getElementById("btn-right");
+let array_card = document.getElementsByClassName("fade");
+
+let option = 1;
+let start_index = 0;
+let end_index = 4;
+let index = 1;
+
+
+
+const hideImages = () => {
+    for (let i = 0; i < array_card.length; i++) {
+        array_card[i].className = "slide-home fade a-hide";
+    }
+};
+
+const showImageleft = () => {
+    hideImages();   
+    index--;
+    if (index <= start_index) {
+        index = end_index;
+    }
+    array_card[index].className = "slide-home fade";
+};
+
+const showImageright = () => {
+    hideImages();
+    index++;
+    if (index > end_index) {
+        index = start_index;
+    }
+    array_card[index].className = "slide-home fade";
+};
+
+
+btn_right.onclick = function () {
+    showImageright();
+}
+
+
+btn_left.onclick = function () {
+    showImageleft();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function initMap() {
@@ -17,10 +85,9 @@ function initMap() {
 
     console.log(myMarker.position)
   
-}
-
-    
+}   
 
 
 window.initMap = initMap;
+
 
